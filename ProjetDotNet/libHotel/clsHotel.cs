152092cc55307b5abede924e843075a0966b8c95
fpelleti <Lumi@10.13.5.237>
2017-02-03 11:11:23 +0100

    creation des lib

---
+++ ProjetDotNet/libHotel/clsHotel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetDotNet.libHotel
+{
+    public class clsHotel
+    {
+        public clsHotel()
+        {
+        }
+
+        public DataSet liste_Hotels(string ville)
+        {
+            SqlConnection MyC = new SqlConnection();
+            MyC.ConnectionString = "Data Source=(local);Initial Catalog=Hotels ;Integrated Security=True";
+            MyC.Open();
+            SqlCommand MyCom = new SqlCommand("sp_getHotels", MyC);
+            MyCom.CommandType = CommandType.StoredProcedure;
+            MyCom.Parameters.Add("@Ville", SqlDbType.NChar);
+            MyCom.Parameters["@Ville"].Value = ville;
+            SqlDataAdapter MyAdapt = new SqlDataAdapter();
+            DataSet myDS = new DataSet();
+            MyAdapt.SelectCommand = MyCom;
+            MyAdapt.Fill(myDS, "liste_hotels");
+            MyAdapt.Dispose();
+            MyCom.Dispose();
+            MyC.Close();
+            return myDS;
+        }
+    }
+}
---
+++ ProjetDotNet/libReservation/clsReservation.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetDotNet.libReservation
+{
+    public class clsReservation
+    {
+        public clsReservation()
+        {
+        }
+
+        public int reservationVol(string nom, string prenom, int idVol)
+        {
+            SqlConnection MyC = new SqlConnection();
+            MyC.ConnectionString = "Data Source=(local);Initial Catalog=Vols ;Integrated Security=True";
+            MyC.Open();
+            SqlCommand MyCom = new SqlCommand("sp_reserverVol", MyC);
+            MyCom.CommandType = CommandType.StoredProcedure;
+            MyCom.Parameters.Add("@Nom", SqlDbType.NChar);
+            MyCom.Parameters["@Nom"].Value = nom;
+            MyCom.Parameters.Add("@Prenom", SqlDbType.NChar);
+            MyCom.Parameters["@Prenom"].Value = prenom;
+            MyCom.Parameters.Add("@IdVol", SqlDbType.Int);
+            MyCom.Parameters["@IdVol"].Value = idVol;
+            int Res = Convert.ToInt32(MyCom.ExecuteScalar());
+            MyCom.Dispose();
+            MyC.Close();
+            return Res;
+        }
+
+        public int reservationHotel(string nom, string prenom, int idHotel)
+        {
+            SqlConnection MyC = new SqlConnection();
+            MyC.ConnectionString = "Data Source=(local);Initial Catalog=Hotels ;Integrated Security=True";
+            MyC.Open();
+            SqlCommand MyCom = new SqlCommand("sp_reserverHotel", MyC);
+            MyCom.CommandType = CommandType.StoredProcedure;
+            MyCom.Parameters.Add("@Nom", SqlDbType.NChar);
+            MyCom.Parameters["@Nom"].Value = nom;
+            MyCom.Parameters.Add("@Prenom", SqlDbType.NChar);
+            MyCom.Parameters["@Prenom"].Value = prenom;
+            MyCom.Parameters.Add("@IdHotel", SqlDbType.Int);
+            MyCom.Parameters["@IdHotel"].Value = idHotel;
+            int Res = Convert.ToInt32(MyCom.ExecuteScalar());
+            MyCom.Dispose();
+            MyC.Close();
+            return Res;
+        }
+    }
+}
